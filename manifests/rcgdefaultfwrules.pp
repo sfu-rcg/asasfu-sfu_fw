371fd7609dad4b306eee93c7f934769ab782ef16
Adam S <asa188@sfu.ca>
2015-05-11 09:46:49 -0700

    Fixed hook

---
+++ manifests/rcgdefaultfwrules.pp
@@ -0,0 +1,54 @@
+class sfu_fw::rcgdefaultfwrules($trustedIPs_nfs = undef, $trustedIPs_rshd = undef, $trustedIPs_5555 = undef) {
+
+  define trust_these_IPs_rshd() {
+    firewall { "004 accept rshd trusted IPs ${name}":
+      proto   => 'tcp',
+      dport   => '514',
+      action  => 'accept',
+      source  => $name, 
+    }  
+  }
+/*  firewall { '005 deny all remaining untrusted to rshd':
+    proto   => 'tcp',
+    dport   => '514',
+    action  => 'drop',
+  }
+*/  
+  define trust_these_IPs_nfs_tcp() {
+    firewall { "006 accept tcp NFS connections from these trusted IPs ${name}":
+      proto   => 'tcp',
+      action  => 'accept',
+      source  => $name, 
+      dport   => [ '111', '2049', '4001', '4002', '4003', '4004' ],
+    }
+  }  
+  define trust_these_IPs_nfs_udp() {
+    firewall { "007 accept udp NFS connections from these trusted IPs ${name}":
+      proto   => 'udp',
+      action  => 'accept',
+      source  => $name, 
+      dport   => [ '111', '2049', '4001', '4002', '4003', '4004' ],
+    }
+  }  
+  define trust_these_5555() {
+    firewall { "008 accept port 5555 from these trusted IPs ${name}":
+      proto   => 'tcp',
+      action  => 'accept',
+      source  => $name, 
+      dport   => '5555',
+    }
+  }  
+  if $trustedIPs_nfs {
+    validate_array($trustedIPs_nfs)  
+    trust_these_IPs_nfs_tcp{$trustedIPsnfs:}
+    trust_these_IPs_nfs_udp{$trustedIPsnfs:}
+  }
+  if $trustedIPs_rshd {
+    validate_array($trustedIPs_rshd)  
+    trust_these_IPs_rshd{$trustedIPsrshd:}
+  }
+  if $trustedIPs_5555 {
+    validate_array($trustedIPs_5555)  
+    trust_these_5555{$trusted5555:}
+  }
+}
