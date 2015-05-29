81314c6cf90f712f256c3f061096bff0fbc6c7cb
Adam S <asa188@sfu.ca>
2015-05-29 16:26:57 -0700

    Decided to use array suffix to create unique resource and then delete() on the other end to clean the

--- manifests/sourceip/simpleloop.pp
+++ manifests/sourceip/simpleloop.pp
@@ -1,26 +1,12 @@
 define sfu_fw::sourceip::simpleloop($filternum = '010', $servicename = undef, $trustedIPs = undef, $proto, $dport = undef, $sport = undef) {
 
- # define trust_these_IPs($filternum = '010', $servicename = undef, $proto, $dport = undef, $sport = undef) {
- #   firewall { "${filternum} accept ${proto} ${servicename} connections from these trusted IPs ${name}":
- #     proto   => $proto,
- #     action  => 'accept',
- #     source  => $name, 
- #     dport   => $dport,
- #     sport   => $sport,
- #   }
- # }  
   if $trustedIPs {
     validate_array($trustedIPs)
     validate_re($filternum, '^[0-9]{3}$', 'Your filternum has to contain a number from 000 to 999 as it is used to organize IPTables rules')
     validate_re($proto,'^[a-zA-Z0-9]+$', 'Apply a proper protocol name to val $proto')
     unless $trustedIPs == [] {
-      $IPs_count = inline_template('<%- count = 0 -%><%- trustedIPs.each do |source_key| -%><%= count -%>,<%- count = count.to_i + 1 -%><%- end -%>')
-      $IPs_count_array = split($IPs_count, ',')
-      
-      #$simpleloopd_name = $IPs_count $servicename $proto $filternum
-
-      sfu_fw::sourceip::simpleloopd { "${IPs_count} ${servicename} ${proto} ${filternum}":
-        trustedIP   => $trustedIPs[inline_template('<%= @IPs_count_array.shift -%>')],
+      $suffix_trustedIPs = suffix($trustedIPs, " ${servicename}${proto}${filternum}")
+      sfu_fw::sourceip::simpleloopd { $suffix_trustedIPs:
         filternum   => $filternum,
         servicename => $servicename,
         proto       => $proto,
--- manifests/sourceip/simpleloopd.pp
+++ manifests/sourceip/simpleloopd.pp
@@ -1,4 +1,5 @@
-define sfu_fw::sourceip::simpleloopd($trustedIP, $filternum = '010', $servicename = undef, $proto, $dport = undef, $sport = undef) {
+define sfu_fw::sourceip::simpleloopd($filternum = '010', $servicename = undef, $proto, $dport = undef, $sport = undef) {
+  $trustedIP = delete($name, " ${servicename}${proto}${filternum}")
   firewall { "${filternum} accept ${proto} ${servicename} connections from these trusted IPs ${trustedIP}":
     proto   => $proto,
     action  => 'accept',
