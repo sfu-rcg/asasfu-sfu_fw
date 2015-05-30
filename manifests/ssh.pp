1212160b4b6152629fd6925cfe6bacdf31dfb958
Adam S <asa188@sfu.ca>
2015-05-29 20:57:35 -0700

    Defining SSH more specifically and utilizing loop capability, fixed if not statement

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -2,17 +2,18 @@
 # If $public is set to true we will still source the trusted networks and create the rules as well as adding allow all SSH
 # as this will allow us to choose to do rate limiting and other options against untrusted networks.
 class sfu_fw::ssh(
-  $public     = false, 
+  $public     = false,
   $trustedIPs = undef,
   ) {
 
   $filternum   = '022'
-  $servicename = 'SSH' 
-  $dport       = 22 
+  $servicename = 'SSH'
+  $dport       = 22
   $sport       = undef
-  $proto       = 'tcp' 
-  if $trustedIPs and !$public {
+  $proto       = 'tcp'
+  if ($trustedIPs) and ($trustedIPs != []) and (!$public) {
     sfu_fw::sourceip::simpleloop { "SSH Name-unused but must be dynamic-Private":
+      trustedIPs  => $trustedIPs,
       filternum   => $filternum,
       servicename => $servicename,
       proto       => $proto,
@@ -22,6 +23,7 @@ class sfu_fw::ssh(
   }
   elsif $public {
     sfu_fw::sourceip::simpleloop { "SSH Name-unused but must be dynamic-Public":
+      trustedIPs  => [ '0.0.0.0' ],
       filternum   => $filternum,
       servicename => $servicename,
       proto       => $proto,
@@ -33,17 +35,3 @@ class sfu_fw::ssh(
     fail("You've chosen to make this host not public but failed to include trusted hosts for SSH")
   }
 }
-#class sfu_fw::ssh(
-#  $public = false,
-#  ) {
-#  validate_bool($public)
-#
-#  if $public == true {
-#    notify { "You chose to have your SSH server PUBLIC": }
-#  }
-#  firewall { '022 allow SSH access':
-#    port   => [ '22' ],
-#    proto  => tcp,
-#    action => accept,
-#  }
-#}
